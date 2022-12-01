5aab2fb1d96670a9e632e5afd7a89c24068d6d80
Rupesh8844 <rupeshthakur8844@gmail.com>
2022-12-01 12:33:11 +0530

    added swagger-ui and completed end-to-end project...

--- Online_Cab_Booking_System/src/main/java/com/cab/OnlineCabBookingSystemApplication.java
+++ Online_Cab_Booking_System/src/main/java/com/cab/OnlineCabBookingSystemApplication.java
@@ -3,7 +3,10 @@ package com.cab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class OnlineCabBookingSystemApplication {
 
 	public static void main(String[] args) {
